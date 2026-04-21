75da066ccc3a5d072d7f83c6adf29d96ae74e0db
ntwari-egide <ntwariegide2@gmail.com>
2026-04-21 18:37:32 -0400

    chore: add dotenv configuration for environment variables in migration and seed scripts

--- server/db/migrate.js
+++ server/db/migrate.js
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import { query } from './postgres.js'
 
 async function migrate() {
--- server/db/seed.js
+++ server/db/seed.js
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import { query } from './postgres.js'
 import { createHash } from 'crypto'
 
